90ca0526f00a3e07202839c1bd31ffcacacb26ba
rpthibeault <rpthibeault@outlook.com>
2024-05-21 13:42:38 -0400

    read from file and mkdir test folder

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
+#include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 #include <antlr4-runtime.h>
 #include <CLexer.h>
 #include <CParser.h>
 
-int
-main() {
+bool is_c_file(const std::string& f) {
+    return f.length() >= 2 && f.substr(f.size() - 2) == ".c";
+}
 
-    antlr4::ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
+
+int
+main(int argc, char** argv) {
+    if (argc < 2 || !is_c_file(argv[1])) {
+        std::cerr << "Usage: " << argv[0] << " <c file>\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file) {
+        std::cerr << "Error: Could not open file " << argv[1] << "\n";
+        return 1;
+    }
+
+    antlr4::ANTLRInputStream input(file);
 
     CLexer lexer(&input);
 
@@ -24,5 +42,6 @@ main() {
 
     std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
 
+    file.close();
     return 0;
 }
